5f409e98e0cdcc2d8fe377cb9ef47b7ac0741524
Mikael Lundin <mikael.lundin@litemedia.se>
2013-03-11 23:16:18 +0100

    Visualizing the game of life in winforms

---
+++ Source/FSharp/GameOfLife.FSharp.App/LifeForms.fs
@@ -0,0 +1,21 @@
+﻿module LifeForms
+    let private place position lifeForm = 
+        let padx, pady = position
+        lifeForm |> List.map (fun (x, y) -> (x + padx), (y + pady))
+
+    // still lifes
+    let block position = [0, 0; 1, 0; 1, 1; 0, 1] |> place position
+    let beehive position = [0, 0; 1, 1; 2, 1; 3, 0; 2, 1; 1, 1] |> place position
+    let loaf position = [1, 0; 2, 0; 3, 1; 3, 2; 2, 3; 1, 2; 0, 1] |> place position
+    let boat position = [0, 0; 1, 0; 2, 1; 1, 2; 0, 1] |> place position
+
+    // oscillators
+    let blinker position = [0, 0; 1, 0; 2, 0] |> place position
+    let toad position = [0, 1; 1, 1; 1, 0; 2, 1; 2, 0; 3, 0] |> place position
+    let beacon position = [0, 1; 0, 0; 1, 0; 2, 3; 3, 3; 3, 2] |> place position
+    let pulsar position = [2, 0; 3, 0; 4, 0; 8, 0; 9, 0; 10, 0; 0, 2; 5, 2; 7, 2; 12, 2; 0, 3; 5, 3; 7, 3; 12, 3; 0, 4; 5, 4; 7, 4; 12, 4; 2, 5; 3, 5; 4, 5; 8, 5; 9, 5; 10, 5; 2, 7; 3, 7; 4, 7; 8, 7; 9, 7; 10, 7; 0, 8; 5, 8; 7, 8; 12, 8; 0, 9; 5, 9; 7, 9; 12, 9; 0, 10; 5, 10; 7, 10; 12, 10; 2, 12; 3, 12; 4, 12; 8, 12; 9, 12; 10, 12] |> place position
+
+    // spaceships
+    let glider position = [0, 2; 1, 2; 1, 0; 2, 2; 2, 1] |> place position
+    let lwss position = [0, 2; 0, 0; 1, 3; 2, 3; 3, 3; 3, 0; 4, 3; 4, 2; 4, 1] |> place position
+            
---
+++ Source/FSharp/GameOfLife.FSharp.App/Program.fs
@@ -0,0 +1,49 @@
+﻿module App
+open System
+open System.Drawing
+open System.Windows.Forms
+open LifeForms
+
+type Canvas(width : int, height : int, multiplier : int) = 
+    inherit PictureBox(Width = width, Height = height)
+    member this.Draw cells =
+        use gfx = this.CreateGraphics()
+        use pen = new Pen(Color.Black, 1.0 |> float32)
+        cells |> List.iter (fun cell -> this.Draw(gfx, pen, cell))
+
+    member this.Draw(g, pen, cell) = 
+        let x, y = (multiplier * fst cell), (multiplier * snd cell)
+        
+        // draw rectangle
+        g.DrawRectangle(pen, x, y, multiplier, multiplier)        
+
+type Window (width : int, height : int, text : string) =
+    inherit Form(Width = width, Height = height, Text = text, Visible = true)
+
+    member this.Add control : Window = 
+        base.Controls.Add(control)
+        this
+
+let window = 
+    let canvas = new Canvas(400, 400, 10)
+    let window = (new Window(400, 400, "Game of Life")).Add(canvas)
+    
+    let tick = async {
+            let board = ref ((blinker (5, 30)) @ (toad (12, 30)) @ (beacon (20, 30)) @ (pulsar (25, 12)) @ (glider (10, 10)) @ (lwss (1, 1)))
+            while true do
+                board := GameOfLife.Run.next(!board)
+                canvas.Invoke(new MethodInvoker(fun () ->
+                        canvas.Refresh()
+                        canvas.Draw(!board)
+                    )) |> ignore
+                do! Async.Sleep(500)
+        }
+
+    Async.StartAsTask(tick) |> ignore
+    window
+
+
+[<EntryPoint>]
+let main argv = 
+    Application.Run(window);
+    0 // return an integer exit code
